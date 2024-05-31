98975121fe3ffcc6978f04b319db842a9cdb6f88
nexovec <hraccesky@gmail.com>
2024-05-31 23:07:23 +0200

    fetching chart data lazily from the server

--- static/js/custom.esm.js
+++ static/js/custom.esm.js
@@ -3,28 +3,15 @@ import * as echarts from 'echarts';
 let chartDomElems = document.querySelectorAll('#graph');
 
 Array.from(chartDomElems).forEach((elem) => {
-  var chart = echarts.init(elem);
-  var option;
-
-  option = {
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        areaStyle: {}
-      }
-    ]
-  };
+  let chart = echarts.init(elem);
+  fetch("/getData").then(async (response) => {
+    let option = await response.json();
+    console.log("Option:", option);
+    option && chart.setOption(option) && chart.resize();
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
   window.addEventListener('resize', function() {
     chart.resize();
   });
-  option && chart.setOption(option);
 });
